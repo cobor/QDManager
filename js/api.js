795f1679d4d22e94f731c71b1627b847d96a155e
colmo <copenyarrubia@gmail.com>
2014-02-09 03:23:49 +0100

    Carga de la lista del servidor
    -Carga de los archivos desde la api 1h
    -Creadas funciones superchulas que convierten los tamaños a HUMAN y las
    fechas a formato AGAPORNI 1h
    -Reunión en el equipo 2h
    -Descanso 20min

--- js/api.js
+++ js/api.js
@@ -1,17 +1,58 @@
-//Cargar lista de archivos y la coloca en la tabla.
-function getFilelistKS1() 
-{
-    $.getJSON('http://api.bots.tf/gala/',function(data){
-            console.log(data[0]);
+$(document).ready(function() {
+
+    //Human FileSizes
+    function getReadableFileSizeString(fileSizeInBytes) {
+
+        var i = -1;
+        var byteUnits = [' kB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
+        do {
+            fileSizeInBytes = fileSizeInBytes / 1024;
+            i++;
+        } while (fileSizeInBytes > 1024);
+
+        return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+    }
+    
+    //Human date
+    function getDateHuman(timestamp){
+        var d = new Date(timestamp*1000);
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+
+        return [day, month, year].join('/');
+    }
+
+    //Cargar lista de archivos y la coloca en la tabla.
+    function getFilelistKS1(refresh=false) 
+    {
+        //Si es una refresco comprobamos si es kas naranja o limon.
+        if(refresh) {
+            $("#ficheros-remotos").empty();
+        }
+        $.getJSON('http://api.bots.tf/gala/',function(data){
+            data.forEach(function(item) {
+                 $("#ficheros-remotos").append('<li class="list-group-item"><span id="filename">'+item["name"]+'</span><span id="filedate" class="badge">'+getDateHuman(item["date"])+'</span><span id="filesize" class="badge">'+getReadableFileSizeString(item["size"])+'</span></li>');
+            });
+        });
+    }
+
+    //Refresh KS1
+    $("#refresh-ks1").click(function(){
+        getFilelistKS1(true);
     });
-}
 
-//Disabling Cache
-$.ajaxSetup({ cache: false });
+    //Disabling Cache
+    $.ajaxSetup({ cache: false });
+
+    //Carga inicial.
+    getFilelistKS1();
+});
 
 
-//Carga inicial.
-getFilelistKS1();
 
 
 
