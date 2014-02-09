dc60aad15c31646649b9595337a465d3adf14e94
cobor <prueba@prueba.com>
2014-02-09 17:28:14 +0100

    MOAR CAMBIOS

--- js/api.js
+++ js/api.js
@@ -4,47 +4,44 @@ $(document).ready(function() {
 //////////////Codigo draggable anterior
 //
 /////////// Estilo cola 
-//        var estilo_cola = "";
-//        
-//        
-//      $( "#ficheros-remotos li" ).draggable({
-//        appendTo: "body",
-//        helper: "clone"
-//      });  
-//        
-//        $( "#cola-descarga " ).droppable({
-//            activeClass: "ui-state-default",
-//            hoverClass: "ui-state-hover",
-//            accept: ":not(.ui-sortable-helper)",
-//            drop: function( event, ui ) {
-//                $( this ).find( ".placeholder" ).remove();
-//                
-//                
-//                $( "<li class='list-group-item'></li>" ).html("<span style='' id='remove-item' class='glyphicon glyphicon-remove-circle'></span>"+ui.draggable.html()+estilo_cola ).appendTo( this );
-//                
-//        }
-//        }).sortable({
-//            items: "li:not(.placeholder)",
-//            sort: function() {
-//                // gets added unintentionally by droppable interacting with sortable
-//                // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
-//                $( this ).removeClass( "ui-state-default" );
-//            }
-//        });    
+      var estilo_cola = "";       
+      $( "#ficheros-remotos li" ).draggable({
+            appendTo: "body",
+            helper: "clone",
+            cancel: "button", 
+            revert: "invalid",
+      });   
+    
+        $( "#cola-descarga " ).droppable({
+            activeClass: "ui-state-default",
+            hoverClass: "ui-state-hover",
+            accept: ":not(.ui-sortable-helper)",
+            drop: function( event, ui ) {
+                $( this ).find( ".placeholder" ).remove();
+                $( "<li class='list-group-item'></li>" ).html("<span style='' id='remove-item' class='glyphicon glyphicon-remove-circle'></span>"+ui.draggable.html()+estilo_cola ).appendTo( this );       
+            }
+        }).sortable({
+            items: "li",
+            sort: function() {
+                // gets added unintentionally by droppable interacting with sortable
+                // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+                $( this ).removeClass( "ui-state-default" );
+            }
+        });    
         
-        // Codigo nuevo draggable
-        
-        function anadirElementos(ui) {
-           // console.log(ui);
-       //    var nombre_fichero = $(ui).find("#filename").text();
-//            var fecha_fichero = $(ui).find("#filedate").text();
-//            var tamano_fichero = $(ui).find("#filesize").text();
-//            
-      //      console.log(nombre_fichero);
-//            console.log(fecha_fichero);
-//            console.log(tamano_fichero);
-        
-            // MIERDA Y MAS MIERDA
+//        // Codigo nuevo draggable
+//        
+//        function anadirElementos(ui) {
+//           // console.log(ui);
+//       //    var nombre_fichero = $(ui).find("#filename").text();
+////            var fecha_fichero = $(ui).find("#filedate").text();
+////            var tamano_fichero = $(ui).find("#filesize").text();
+////            
+//      //      console.log(nombre_fichero);
+////            console.log(fecha_fichero);
+////            console.log(tamano_fichero);
+//        
+//            // MIERDA Y MAS MIERDA
 //            $("#ficheros-remotos").empty().append(
 //                
 //                "<li class='list-group-item'>"+
@@ -53,37 +50,37 @@ $(document).ready(function() {
 //                    "<span id='filesize' class='badge'>14 GB</span>"+
 //                "</li>"
 //            );
-            getFilelistKS1();
-            
-        }
-        
-        var $ficheros_remotos = $( "#ficheros-remotos" );
-	    var $cola_descargas = $( "#cola-descarga" );
-        
-        
-        $( "li", $ficheros_remotos ).draggable({
-             appendTo: "body",
-            cancel: "button", 
-		      revert: "invalid",
-		  containment: "document",
-		  helper: "clone",
-		  cursor: "move"
-	   });
-      $cola_descargas.droppable({
-		  accept: "#ficheros-remotos > li",
-		  drop: function( event, ui ) {
-              $("#cola-descarga").append(ui.draggable); // AQUI DE ALGUNA FORMA HAY QUE GENERAR EL CODIGO NECESARIO PARA AÑADIR LA BARRA Y LOS ESTILOS INDIVIDUALES QUE TIENE LA COLA
-			  anadirElementos(); // esto hace algun tipo de magia que no se borre lo de la izquierda
-              $( "li", $ficheros_remotos ).draggable({
-    		      cancel: "button", // these elements won't initiate dragging
-		          revert: "invalid", // when not dropped, the item will revert back to its initial position
-		          containment: "document",
-		          helper: "clone",
-		          cursor: "move"
-	           })
-	       }
-	   });    
-    
+//            getFilelistKS1();
+//            
+//        }
+//        
+//        var $ficheros_remotos = $( "#ficheros-remotos" );
+//	    var $cola_descargas = $( "#cola-descarga" );
+//        
+//        
+//        $( "li", $ficheros_remotos ).draggable({
+//             appendTo: "body",
+//            cancel: "button", 
+//		      revert: "invalid",
+//		  containment: "document",
+//		  helper: "clone",
+//		  cursor: "move"
+//	   });
+//      $cola_descargas.droppable({
+//		  accept: "#ficheros-remotos > li",
+//		  drop: function( event, ui ) {
+//              $("#cola-descarga").append(ui.draggable); // AQUI DE ALGUNA FORMA HAY QUE GENERAR EL CODIGO NECESARIO PARA AÑADIR LA BARRA Y LOS ESTILOS INDIVIDUALES QUE TIENE LA COLA
+//			  anadirElementos(); // esto hace algun tipo de magia que no se borre lo de la izquierda
+//              $( "li", $ficheros_remotos ).draggable({
+//    		      cancel: "button", // these elements won't initiate dragging
+//		          revert: "invalid", // when not dropped, the item will revert back to its initial position
+//		          containment: "document",
+//		          helper: "clone",
+//		          cursor: "move"
+//	           })
+//	       }
+//	   });    
+//    
     
     
     
